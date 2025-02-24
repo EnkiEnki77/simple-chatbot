a65fc9c2881e54b807a17527a96ac57d8b5bb07d
Enki Layman <layman212@gmail.com>
2025-02-24 05:02:42 -0500

    Learned how to format strings with printf

--- Chatbot.java
+++ Chatbot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;;
+
 public class Chatbot {
     public static void main(String[] args) {
         String botName = "Anu";
@@ -5,6 +7,18 @@ public class Chatbot {
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + yearBorn + ".");
+
+        Scanner sc = new Scanner(System.in);
+
+        // Next allows us to capture keyboard input, even numbers. But it converts
+        // everything to a string.
+        String name = sc.next();
+        // If you want to print a compound string (one with spaces) you have to invoke a
+        // new next. Next only reads characters up to a space.
+        String lname = sc.next();
+        System.out.println("Hello, " + name + " " + lname);
+
+        sc.close();
 
     }
 }
---
+++ FormatStrings.java
@@ -0,0 +1,10 @@
+public class FormatStrings {
+    public static void main(String[] args) {
+        double length = 54.4;
+        double width = 42.3;
+
+        double area = length * width;
+
+        System.out.printf("%.2f", area);
+    }
+}
