ba0dd05a8a878b68dd4347cf51968a4ff13fbef0
Enki Layman <layman212@gmail.com>
2025-02-24 02:27:23 -0500

    chatbot greeting message

---
+++ Chatbot.java
@@ -0,0 +1,10 @@
+public class Chatbot {
+    public static void main(String[] args) {
+        String botName = "Anu";
+        int yearBorn = 2025;
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + yearBorn + ".");
+
+    }
+}
